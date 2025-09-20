cfaf6b49388c687d5b00e88102c6d77a7bf9c6b1
Binamra Lamsal <binamralamsal@gmail.com>
2025-09-20 20:35:11 +0545

    asdf

--- Tune/utils/decorators/play.py
+++ Tune/utils/decorators/play.py
@@ -122,7 +122,7 @@ def PlayWrapper(command):
             userbot = await get_assistant(chat_id)
             try:
                 try:
-                    async for dialog in app.get_dialogs(): pass
+                    async for dialog in userbot.get_dialogs(): pass
 
                     member = await app.get_chat_member(chat_id, userbot.id)
                 except ChatAdminRequired:
