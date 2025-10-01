f03faabf2effbbdb596c1bf54e030813a2990ee0
Bisu G <lannsnolo@gmail.com>
2025-10-02 00:29:15 +0545

    Aaloo Khaoge?

--- Tune/core/userbot.py
+++ Tune/core/userbot.py
@@ -91,7 +91,7 @@ class Userbot:
             LOGGER(__name__).error(f"Failed to start Assistant {index}: {e}")
 
     async def start(self):
-        LOGGER(__name__).info("Starting Tune's Assistants...")
+        LOGGER(__name__).info("Aaloo Khaoge?")
         await self.start_assistant(self.one, 1)
         await self.start_assistant(self.two, 2)
         await self.start_assistant(self.three, 3)
