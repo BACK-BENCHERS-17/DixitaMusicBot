f2eb8283c81f61f5d4b18a165466f50917368679
Bisu G <lannsnolo@gmail.com>
2025-10-02 06:24:27 +0545

    Update start.py

--- Tune/utils/inline/start.py
+++ Tune/utils/inline/start.py
@@ -17,10 +17,13 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_7"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_5"], url=config.UPSTREAM_REPO),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_3"], callback_data="open_help"),
-            InlineKeyboardButton(text=_["S_B_5"], url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(text=_["S_B_3"], callback_data="open_help"),            
+        ],
+        [            
+            InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_CHAT),            
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHANNEL),
         ],
     ]
